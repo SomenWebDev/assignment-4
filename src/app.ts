b9706f0081a5fea506dc222820c0ed66bcc94815
Somen Singha <somensingha18@gmail.com>
2026-07-31 12:05:27 +0600

    Project setup done

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import cookieParser from "cookie-parser";
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+
+const app: Application = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World?");
+});
+
+export default app;
