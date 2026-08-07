455310d1dbf181b2bf9eb78975e280fd4509ce6f
Somen Singha <somensingha18@gmail.com>
2026-08-07 18:04:39 +0600

    review controller and service done

---
+++ src/modules/review/review.controller.ts
@@ -0,0 +1,19 @@
+import type { Request, Response } from "express";
+import httpStatus from "http-status";
+
+import { createReview } from "./review.service";
+import { createReviewSchema } from "./review.validation";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendRsponse";
+
+export const addReview = catchAsync(async (req: Request, res: Response) => {
+  const input = createReviewSchema.parse(req.body);
+  const review = await createReview(req.user!.id, input);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message: "Review submitted successfully",
+    data: review,
+  });
+});
